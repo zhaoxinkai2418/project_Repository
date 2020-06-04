7a1ec42cf9573da63706aaa672996274ab81b842
zhaoxinkai2418 <tt_zhaoxinkai@163.com>
2020-06-04 08:45:50 +0800

    第一次提交

---
+++ demo/src/com/zxk/student.java
@@ -0,0 +1,22 @@
+package com.zxk;
+
+public class student {
+    String name="zxk";
+    int age = 22;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
